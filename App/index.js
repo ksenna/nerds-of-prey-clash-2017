0ed4df6467e3c448bef0b7ca1e07862389a630ec
Kelly Senna <jkellysenna@gmail.com>
2017-05-04 17:22:21 -0400

    Change bkgrd color and remove unused styles on index.js

--- App/index.js
+++ App/index.js
@@ -20,16 +20,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: '#FFFFFF',
+  }
 });
