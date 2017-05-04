4318a0650b44d81750ccc314b18dfcdf95da14fe
David Greenhalgh <davidrgreenhalgh@gmail.com>
2017-05-03 21:32:51 -0400

    Defer views to App/index.js in index.android for better code sharing across platform

---
+++ App/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import ClockControl from './Components/ClockControl';
+
+export default class NerdsOfPrey extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <ClockControl/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
