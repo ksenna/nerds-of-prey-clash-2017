ef9f61e4898221cd8005fbc8bc57a7f641c8caa8
Kelly Senna <jkellysenna@gmail.com>
2017-05-04 15:35:07 -0400

    Add Autocomplete component

---
+++ App/Components/Autocomplete.js
@@ -0,0 +1,210 @@
+import React, { Component, PropTypes } from 'react';
+import {
+  ListView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from 'react-native';
+
+export default class Autocomplete extends Component {
+  static propTypes = {
+    ...TextInput.propTypes,
+    /**
+     * These styles will be applied to the container which
+     * surrounds the autocomplete component.
+     */
+    containerStyle: View.propTypes.style,
+    /**
+     * Assign an array of data objects which should be
+     * rendered in respect to the entered text.
+     */
+    data: PropTypes.array,
+    /*
+     * These styles will be applied to the container which surrounds
+     * the textInput component.
+     */
+    inputContainerStyle: View.propTypes.style,
+    /*
+     * These styles will be applied to the container which surrounds
+     * the result list.
+     */
+    listContainerStyle: View.propTypes.style,
+    /**
+     * These style will be applied to the result list.
+     */
+    listStyle: ListView.propTypes.style,
+    /**
+     * `onShowResults` will be called when list is going to
+     * show/hide results.
+     */
+    onShowResults: PropTypes.func,
+    /**
+     * `renderItem` will be called to render the data objects
+     * which will be displayed in the result view below the
+     * text input.
+     */
+    renderItem: PropTypes.func,
+    /**
+     * `renderSeparator` will be called to render the list separators
+     * which will be displayed between the list elements in the result view
+     * below the text input.
+     */
+    renderSeparator: PropTypes.func,
+    /**
+     * renders custom TextInput. All props passed to this function.
+     */
+    renderTextInput: PropTypes.func
+  };
+
+  static defaultProps = {
+    data: [],
+    defaultValue: '',
+    renderItem: rowData => <Text>{rowData}</Text>,
+    renderSeparator: null,
+    renderTextInput: props => <TextInput {...props} />
+  };
+
+  constructor(props) {
+    super(props);
+
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = { dataSource: ds.cloneWithRows(props.data) };
+  }
+
+  componentWillReceiveProps({ data }) {
+    const dataSource = this.state.dataSource.cloneWithRows(data);
+    this.setState({ dataSource });
+  }
+
+  /**
+   * Proxy `blur()` to autocomplete's text input.
+   */
+  blur() {
+    const { textInput } = this;
+    textInput && textInput.blur();
+  }
+
+  /**
+   * Proxy `focus()` to autocomplete's text input.
+   */
+  focus() {
+    const { textInput } = this;
+    textInput && textInput.focus();
+  }
+
+  renderResultList() {
+    const { dataSource } = this.state;
+    const { listStyle, renderItem, renderSeparator } = this.props;
+
+    return (
+      <ListView
+        dataSource={dataSource}
+        keyboardShouldPersistTaps="always"
+        renderRow={renderItem}
+        renderSeparator={renderSeparator}
+        style={[styles.list, listStyle]}
+      />
+    );
+  }
+
+  renderTextInput() {
+    const { onEndEditing, renderTextInput, style } = this.props;
+    const props = {
+      style: [styles.input, style],
+      ref: ref => (this.textInput = ref),
+      onEndEditing: e => onEndEditing && onEndEditing(e),
+      ...this.props
+    };
+
+    return renderTextInput(props);
+  }
+
+  render() {
+    const { dataSource } = this.state;
+    const {
+      containerStyle,
+      inputContainerStyle,
+      listContainerStyle,
+      onShowResults
+    } = this.props;
+    const showResults = dataSource.getRowCount() > 0;
+
+    // Notify listener if the suggestion will be shown.
+    onShowResults && onShowResults(showResults);
+
+    return (
+      <View style={[styles.container, containerStyle]}>
+        <View style={[styles.inputContainer, inputContainerStyle]}>
+          {this.renderTextInput()}
+        </View>
+        <View style={listContainerStyle}>
+          {showResults && this.renderResultList()}
+        </View>
+      </View>
+    );
+  }
+}
+
+const border = {
+  borderBottomColor: '#b9b9b9',
+  borderBottomWidth: 1
+};
+
+const androidStyles = {
+  container: {
+    flex: 1
+  },
+
+  inputContainer: {
+    ...border,
+    marginBottom: 0
+  },
+
+  list: {
+    ...border,
+    backgroundColor: 'white',
+    borderTopWidth: 0,
+    margin: 10,
+    marginTop: 0
+  }
+};
+
+const iosStyles = {
+  container: {
+    zIndex: 1
+  },
+
+  inputContainer: {
+    ...border
+  },
+
+  input: {
+    backgroundColor: 'white',
+    height: 40,
+    paddingLeft: 3
+  },
+
+  list: {
+    ...border,
+    backgroundColor: 'white',
+    borderTopWidth: 0,
+    left: 0,
+    position: 'absolute',
+    right: 0
+  }
+};
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: 'white',
+    height: 40,
+    paddingLeft: 3
+  },
+  
+  ...Platform.select({
+    android: { ...androidStyles },
+    ios: { ...iosStyles }
+  })
+});
