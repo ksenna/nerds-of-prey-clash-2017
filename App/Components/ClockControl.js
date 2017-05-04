730182dcb203e4c5e2b5c6daa2001807c290b5bd
David Greenhalgh <davidrgreenhalgh@gmail.com>
2017-05-03 21:22:34 -0400

    Added basic ClockControl component as an exploration of React components

---
+++ App/Components/ClockControl.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class ClockControl extends Component {
+  render() {
+    return(
+      <View>
+        <Text>'Hello'</Text>
+      </View>
+    );
+  }
+}
