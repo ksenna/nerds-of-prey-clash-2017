b401482274a9b2c3bcdeda6b9beea71a3f8f31b2
Kelly Senna <jkellysenna@gmail.com>
2017-05-04 20:22:59 -0400

    Create Images directory and add start/stop pngs

--- App/Themes/index.js
+++ App/Themes/index.js
@@ -1,6 +1,7 @@
 import Colors from './Colors'
 import Fonts from './Fonts'
+import Images from './Images'
 import Metrics from './Metrics'
 import ApplicationStyles from './ApplicationStyles'
 
-export { Colors, Fonts, Metrics, ApplicationStyles }
+export { Colors, Fonts, Images, Metrics, ApplicationStyles }
