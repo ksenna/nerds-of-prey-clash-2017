970c3c55c5b7efe6e177ecde12c3f76e0f07fad7
David Greenhalgh <davidrgreenhalgh@gmail.com>
2017-05-04 23:55:09 -0400

    Cleaned up debug buttons

--- App/Navigation/TabView.js
+++ App/Navigation/TabView.js
@@ -20,22 +20,12 @@ const TabView = (props, context) => {
   const drawer = context.drawer;
   return (
     <View style={[styles.container, props.sceneStyle ]}>
-      <Text>Tab {props.title}</Text>
       {props.name === 'tab1_1' &&
-        <View>
-          <Button onPress={Actions.tab2_1} title="next screen for pomodoro" />
-          <Stopwatch/>
-        </View>
+        <Stopwatch/>
       }
       {props.name === 'tab2_1' &&
-        <View>
-          <Button onPress={Actions.tab2_2} title="next screen for tab2_2" />
-          <PomodoroTImer/>
-        </View>
+        <PomodoroTImer/>
       }
-      <Button onPress={Actions.pop} title="Back" />
-      <Button onPress={Actions.tab1} title="Switch to tab1" />
-      <Button onPress={Actions.tab2} title="Switch to tab2" />
     </View>
   );
 };
