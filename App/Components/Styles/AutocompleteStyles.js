11c56f17cf2a45d443bf44535ca658624eebf80a
Kelly Senna <jkellysenna@gmail.com>
2017-05-05 09:18:15 -0400

    Create stylesheet for Autocomplete component

---
+++ App/Components/Styles/AutocompleteStyles.js
@@ -0,0 +1,56 @@
+import { Platform, StyleSheet } from 'react-native'
+import { Colors, Fonts } from '../../Themes/'
+
+const border = {
+  borderBottomColor: Colors.greyish,
+  borderBottomWidth: 0.5
+};
+
+export default StyleSheet.create({
+  container: {
+    ...Platform.select({
+      ios: {
+        zIndex: 1
+      },
+      android: {
+        flex: 1
+      }
+    })
+  },
+
+  input: {
+    backgroundColor: 'white',
+    paddingLeft: 3,
+    fontSize: Fonts.size.small,
+    color: Colors.black,
+    ...Platform.select({
+      ios: {
+        height: 25,
+      },
+      android: {
+        height: 40
+      }
+    })
+  },
+  
+  inputContainer: {
+    ...border,
+  },
+
+  list: {
+    ...border,
+    backgroundColor: 'white',
+    borderTopWidth: 0,
+    ...Platform.select({
+      ios: {
+        left: 0,
+        position: 'absolute',
+        right: 0
+      },
+      android: {
+        margin: 10,
+        marginTop: 0
+      }
+    })
+  }
+})
