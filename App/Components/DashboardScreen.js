325dbccaaa2e52b13608dcfca89f9020e78c73dc
David Greenhalgh <davidrgreenhalgh@gmail.com>
2017-05-05 16:53:36 -0400

    Added list view with simple list item

--- App/Components/DashboardScreen.js
+++ App/Components/DashboardScreen.js
@@ -1,16 +1,28 @@
 import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ListView } from 'react-native';
 import styles from '../Navigation/Styles/TabViewStyles';
+import ActivityListItem from './ActivityListItem';
 
 export default class DashboardScreen extends Component {
   constructor(props) {
     super(props);
+
+    const dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 != r2});
+    this.state = {
+      activityDataSource: dataSource.cloneWithRows([
+        'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+      ])
+    };
   }
   
   render() {
     return(
       <View style={[styles.container, this.props.sceneStyle ]}>
-        <Text>TODO</Text>
+        <Text>Recent Activity</Text>
+        
+        <ListView
+          dataSource={this.state.activityDataSource}
+          renderRow={(rowData) => <ActivityListItem rowData={rowData}/>}/>
       </View>
     );
   }
