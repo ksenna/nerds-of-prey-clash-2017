dc94d748e0634e49630f7232b3db1e0c0e1751a5
Kelly Senna <jkellysenna@gmail.com>
2017-05-05 01:16:11 -0400

    Add conditional component rendering

--- App/Components/Clock.js
+++ App/Components/Clock.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Platform, View } from 'react-native';
 import ClockControl from './ClockControl';
 import Timer from './Timer';
 import moment from 'moment';
+import IosTagInput from './IosTagInput'
+import AndroidTagInput from './AndroidTagInput'
 
 const timer = require('react-native-timer');
 
@@ -20,10 +22,17 @@ export default class Clock extends Component {
     this.onTimerToggled = this.onTimerToggled.bind(this);
   }
 
+  getTagInputComponent() {
+    var tagComponent = (Platform.OS === 'ios') ? <IosTagInput /> : <AndroidTagInput />
+    // this.props.running ? Images.stopButton : Images.startButton
+    return tagComponent
+  }
+
   render() {
     return(
       <View>
         <Timer timeElapsed={this.state.timeElapsed}/>
+        {this.getTagInputComponent()}
         <ClockControl
           running={this.state.running}
           toggleTimer={this.onTimerToggled}/>
