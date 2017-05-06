85d33932947589af225aeacfb1dabb5c2a7a3f3d
David Greenhalgh <davidrgreenhalgh@gmail.com>
2017-05-06 17:46:34 -0400

    fix warning

--- App/Components/DashboardScreen.js
+++ App/Components/DashboardScreen.js
@@ -40,7 +40,8 @@ class DashboardScreen extends Component {
         <Text style={dashboardScreenStyles.recentActivityHeader}>RECENT ACTIVITY</Text>
         <ListView
           dataSource={this.state.activityDataSource}
-          renderRow={this.populateActivityListItem}/>
+          renderRow={this.populateActivityListItem}
+          enableEmptySections={true}/>
       </View>
     );
   }
